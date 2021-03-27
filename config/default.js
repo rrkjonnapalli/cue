091659ae7ca76a85c8200041f22f76ed59d239eb
rrkjonnapalli <r.ravikiranjonnapalli@gmail.com>
2021-03-27 09:41:46 +0530

    updated app with basic routers

--- config/default.js
+++ config/default.js
@@ -3,5 +3,11 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   HOST: process.env.HOST || '0.0.0.0',
   PORT: process.env.PORT || 8089,
-  TIMESTAMP_IGNORE_MODELS: (process.env.TIMESTAMP_IGNORE_MODELS || '').split(',')
+  API_VERSION: process.env.API_VERSION || '1',
+  TIMESTAMP_IGNORE_MODELS: (process.env.TIMESTAMP_IGNORE_MODELS || '').split(','),
+  APP_DB_TYPE: 'SQLite', /** SQLite, pg */
+  APP_DB_CONFIG: {
+    STORAGE: 'data.db'
+  },
+  POSTGRES: {}
 };
